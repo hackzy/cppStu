50b155c8ae8021bc6aee2d45054dfff6acb58e4a
hackzy <819387214@qq.com>
2023-04-22 21:23:59 +0800

    changes not staged for commit: modified: cpp.cpp modified: cpp.exe

--- cpp.cpp
+++ cpp.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
-
+int sho(int arry);
 int main() {
 //打印99乘法表
 for (int i = 9; i >= 1; i--) {
     for (int j = 1; j <= i; j++) {
         cout << j << "*" << i << "=" << i * j << " ";
     }
-    cout << endl;
+    cout << sho(30)<< endl;
 }
 
     return 0;
+}
+
+int sho(int arry){
+    retun ++arry
 }
